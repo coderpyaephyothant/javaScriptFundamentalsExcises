5a422da77e483106f7b4c6ef46e050a35b1536c6
pyae phyo thant <pyaephyothantengr520@gmail.com>
2023-07-24 21:25:25 +0630

    excise for javaScript

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// ['a', 'b', 'c'].forEach(function callback(v) {
+//     console.log(v);
+//   });
+
+//   const myArr = ['a', 'b', 'c'];
+//   myArr.forEach((item,index)=>{
+//     console.log('kyayy ohh c jat',item,index);
+//   } )
+
+// const myArr = ['kyay oh', 'pork sweet curry', 'bread'];
+// function LogItemAndindex(item,index){
+//     console.log(`hello i am ${item}. I have index ${index}.`);
+// };
+// myArr.forEach(LogItemAndindex);
+
+// const foods = ['spicy chicken','sweet potato','pork bbq'];
+
+// function itemIndexIs(item,index){
+// console.log(   `my favorite food. Number ${index +1 } is ${item}.`)
+// };
+// foods.forEach(itemIndexIs);
+
+// const myType = true;
+// switch (typeof myType) {
+//     case 'string':
+//         console.log(`${myType} type is String. `)
+//         break;
+//         case 'boolean':
+//             console.log(`${myType} type is boolean. `)
+//             break;
+//             case 'number':
+//                 console.log(`${myType} type is number. `)
+//                 break;
+//     default:
+//         console.log(`it may be undefined.`)
+//         break;
+// };
+
+// const yourScore = 79;
+// switch (true) {
+//     case yourScore >= 80:
+//         console.log(`${yourScore} is Excellent.`)
+//         break;
+//     case yourScore >=60:
+//         console.log(`${yourScore} is Good.`)
+//         break;
+//         case yourScore >=40:
+//         console.log(`${yourScore} is need to improve.5ting.`);
+//         break;
+//     default: console.log('123');
+//         break;
+// }
